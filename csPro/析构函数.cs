df1b439960144fca5894446808adb997ff3c1703
Fidea <uwenhao2008@qq.com>
2020-12-01 17:21:40 +0800

    新增加书籍学习分支

---
+++ csPro/析构函数.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPro
+{
+    class xigouFunc
+    {
+        public xigouFunc()
+        {
+            Console.WriteLine("进入了构造函数");
+        }
+        ~xigouFunc()
+        {
+            Console.WriteLine("进入了析构函数");
+        }
+        static void Main(string[] args)
+        {
+            xigouFunc xg = new xigouFunc();   // 进入构造函数
+            xg = null;                        // 进入析构函数   实际上不需要这行，对象不使用的时候也会自动析构的  程序员无法直接操作析构函数，只能通过GC来实现
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csPro/类与对象.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPro
+{
+    class Cperson
+    {
+        private string myName;
+        public string Name
+        {
+            get { return myName+"--"; }
+            set { myName = value; }
+        }
+        static void Main(string[] args)
+        {
+            Cperson tom = new Cperson();
+            tom.Name = "Tom";
+            Console.WriteLine(tom.Name);
+            Console.WriteLine(tom.myName);
+            Console.ReadLine();
+        }
+    }
+}
